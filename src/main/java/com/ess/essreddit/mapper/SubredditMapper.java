6e95eb3bca2ae00624baef85b758bf7f004961ab
Esraa Abduallah <esraahisham753@gmail.com>
2026-05-30 12:17:30 +0300

    Fix: Fix SubredditMapper and SubredditService

--- src/main/java/com/ess/essreddit/mapper/SubredditMapper.java
+++ src/main/java/com/ess/essreddit/mapper/SubredditMapper.java
@@ -11,14 +11,18 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SubredditMapper {
+    @Mapping(target = "id", source = "subredditId")
+    @Mapping(target = "name", source = "subredditName")
     @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
     SubredditDto mapToSubredditDto(Subreddit subreddit);
 
     default Integer mapPosts(List<Post> subredditPosts) {
-        return subredditPosts.size();
+        return subredditPosts != null ? subredditPosts.size() : 0;
     }
 
     @InheritInverseConfiguration
-    @Mapping(target = "numberOfPosts", ignore = true)
+    @Mapping(target = "posts", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
     Subreddit mapToSubreddit(SubredditDto subredditDto);
 }
--- src/main/java/com/ess/essreddit/services/SubredditService.java
+++ src/main/java/com/ess/essreddit/services/SubredditService.java
@@ -1,15 +1,21 @@
 package com.ess.essreddit.services;
 
 import com.ess.essreddit.dto.SubredditDto;
+import com.ess.essreddit.exceptions.EssRedditException;
 import com.ess.essreddit.mapper.SubredditMapper;
 import com.ess.essreddit.model.Subreddit;
+import com.ess.essreddit.model.User;
 import com.ess.essreddit.repository.SubredditRepository;
+import com.ess.essreddit.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,10 +24,18 @@ import java.util.stream.Collectors;
 public class SubredditService {
     private final SubredditRepository subredditRepository;
     private final SubredditMapper subredditMapper;
+    private final UserRepository userRepository;
 
     @Transactional
     public SubredditDto saveSubreddit(SubredditDto subredditDto) {
         Subreddit subreddit = subredditMapper.mapToSubreddit(subredditDto);
+        String username = Objects.requireNonNull(SecurityContextHolder.getContext()
+                        .getAuthentication())
+                                .getName();
+        User currentUser = userRepository.findUserByUsername(username)
+                        .orElseThrow(() -> new EssRedditException("No current user found with username: " + username));
+        subreddit.setUser(currentUser);
+        subreddit.setCreatedDate(Instant.now());
         subredditRepository.save(subreddit);
         subredditDto.setId(subreddit.getSubredditId());
 
